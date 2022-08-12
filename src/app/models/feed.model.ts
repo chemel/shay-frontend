d19921ffb8cf96b54cf953f64840e3f1efd521a0
Alexis CHEMEL <achemel@openstudio.fr>
2022-08-12 22:00:05 +0200

    [ADD] Ajout des favicons des flux

--- src/app/models/feed.model.ts
+++ src/app/models/feed.model.ts
@@ -1,8 +1,13 @@
-import { Category } from "./category.model";
-
 export class Feed {
   id?: number;
   title?: string;
   url?: string;
   category?: string;
+
+  public get domain(): string {
+    if(this.url === undefined) {
+      return '';
+    }
+    return (new URL(this.url)).hostname;
+  }
 }
