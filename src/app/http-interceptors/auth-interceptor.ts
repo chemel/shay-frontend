2e7384c94d8ba5be2a1735b45fb1dbe0cc923743
Alexis CHEMEL <chemel@users.noreply.github.com>
2022-09-20 21:04:50 +0200

    [ADD] Gestion des requêtes unauthorized

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
-
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     const isApiRequest = req.url.startsWith(environment.backendUrl);
-
     if (isApiRequest) {
       const authToken = this.authService.getAuthorizationToken();
       if(authToken) {
-        const authReq = req.clone({
+        req = req.clone({
           headers: req.headers.set('Authorization', 'Bearer ' + authToken)
         });
-  
-        return next.handle(authReq);
       }
     }
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError(err => {
+        if(err instanceof HttpErrorResponse && err.status === 401) {
+          return this.handleUnauthorized(req, next);
+        }
+        return throwError(() => err);
+      })
+    );
+  }
+
+  private handleUnauthorized(req: HttpRequest<any>, next: HttpHandler) {
+    this.router.navigate(['login']);
+    return EMPTY;
   }
 }
