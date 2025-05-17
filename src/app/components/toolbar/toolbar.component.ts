1e4f28f66a248b8ce2e57d8bdb50678a8ef82f9f
Alexis CHEMEL <chemel@users.noreply.github.com>
2025-05-17 20:50:07 +0200

    Correction routing

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
+  imports: [RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
