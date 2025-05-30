fd50390da59df82849b7c969e7d4f1c173d3d190
Alexis CHEMEL <chemel@users.noreply.github.com>
2025-05-30 16:16:16 +0200

    Add an hamburger menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { FormLoginComponent } from './components/form-login/form-login.component
 import { LayoutNewsreaderComponent } from './components/reader/layout-newsreader/layout-newsreader.component';
 import { NewFeedComponent } from './components/new-feed/new-feed.component';
 import { FeedsListComponent } from './components/admin/feeds-list/feeds-list.component';
+import { CategoriesListComponent } from './components/admin/categories-list/categories-list.component';
 
 export const routes: Routes = [
     { path: 'login', component: FormLoginComponent },
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { path: 'reader', component: LayoutNewsreaderComponent },
     { path: 'new-feed', component: NewFeedComponent },
     { path: 'admin/feeds', component: FeedsListComponent },
+    { path: 'admin/categories', component: CategoriesListComponent },
 ];
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,28 +1,59 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
 import { RouterLink } from '@angular/router';
 import { User } from '@app/models/user.model';
 import { AuthService } from '@app/services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [CommonModule, RouterLink],
+  imports: [NgIf, MenuModule, ButtonModule, RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+  public isUserLogged: boolean = false;
 
-  protected isUserLogged: boolean = false;
+  public items: MenuItem[] = [
+    {
+      label: 'Manage feeds',
+      icon: 'pi pi-list',
+      routerLink: '/admin/feeds'
+    },
+    {
+      label: 'Manage categories',
+      icon: 'pi pi-tags',
+      routerLink: '/admin/categories'
+    },
+    {
+      separator: true
+    },
+    {
+      label: 'Import / Export',
+      icon: 'pi pi-upload',
+      routerLink: '/admin/import-export'
+    },
+    {
+      separator: true
+    },
+    {
+      label: 'Logout',
+      icon: 'pi pi-sign-out',
+      routerLink: '/login'
+    }
+  ];
 
   constructor(
     private authService: AuthService
   ) {
+
+  }
+
+  ngOnInit(): void {
     this.authService.user.subscribe(user => {
       this.isUserLogged = user instanceof User;
     });
   }
-
-  ngOnInit(): void {
-  }
-
 }
